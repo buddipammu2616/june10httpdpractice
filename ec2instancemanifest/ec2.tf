d650d385b7eeca4b02d3fa801f72566e84641f6e
mahesh <maheshkuraganti9@gmail.com>
2024-06-10 18:11:35 +0530

    commited

--- ec2instancemanifest/ec2.tf
+++ ec2instancemanifest/ec2.tf
@@ -41,8 +41,8 @@ resource "aws_route_table_association" "maheshRTA" {
 }
 #create EC2 instance
 resource "aws_instance" "maheshec2" {
-  ami = "ami-00fa32593b478ad6e"
-  instance_type = "t2.micro"
+  ami = var.aws_ami_id
+  instance_type = var.aws_instance_type
   key_name = "terraform-keypair"
   subnet_id = aws_subnet.maheshsubnet.id
   vpc_security_group_ids = [aws_security_group.maheshSG.id]
