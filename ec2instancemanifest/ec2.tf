0ab42ebff8497ee3d4ce54ac9422bf7cb421151c
mahesh <maheshkuraganti9@gmail.com>
2024-06-10 23:33:53 +0530

    commited

--- ec2instancemanifest/ec2.tf
+++ ec2instancemanifest/ec2.tf
@@ -47,7 +47,7 @@ resource "aws_instance" "maheshec2" {
   subnet_id = aws_subnet.maheshsubnet.id
   vpc_security_group_ids = [aws_security_group.maheshSG.id]
   user_data = file("apacheinstall.sh")
-  count = var.aws_instance_count
+  #count = var.aws_instance_count
   tags = {
     "Name" = "maheshec2"
   }
