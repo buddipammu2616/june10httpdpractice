6c56a6772ea7aa3ba9a0b486fb0b106efa534370
mahesh <maheshkuraganti9@gmail.com>
2024-06-10 17:24:27 +0530

    ec2 instance created and commited

---
+++ ec2instancemanifest/ec2.tf
@@ -0,0 +1,41 @@
+#Create a vpc
+resource "aws_vpc" "maheshvpc" {
+    cidr_block = "10.0.0.0/16"
+  tags = {
+    "Name" = "maheshvpc"
+  }
+}
+#Create subnets
+resource "aws_subnet" "maheshsubnet" {
+  vpc_id = aws_vpc.maheshvpc.id
+  availability_zone = "ap-south-1a"
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    "Name" = "maheshsubnet"
+  }
+}
+#Create Internet Gateway
+resource "aws_internet_gateway" "maheshIG" {
+  vpc_id = aws_vpc.maheshvpc.id
+  tags = {
+    "Name" = "MaheshIG"
+  }
+}
+#Create a Route Table
+resource "aws_route_table" "maheshRT" {
+  vpc_id = aws_vpc.maheshvpc.id
+  tags = {
+    "Name" = "maheshRT"
+    }
+  }
+#create a Route
+resource "aws_route" "maheshRoute" {
+  route_table_id = aws_route_table.maheshRT.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.maheshIG.id
+}
+#Associate Route Table With the subnet
+resource "aws_route_table_association" "maheshRTA" {
+  subnet_id = aws_subnet.maheshsubnet.id
+  route_table_id = aws_route_table.maheshRT.id
+}
