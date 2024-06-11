d41d33210a08b784e55775e520fe014df7e277ff
mahesh <maheshkuraganti9@gmail.com>
2024-06-11 12:26:58 +0530

    again db commited

--- dbmanifest/rdsdb.tf
+++ dbmanifest/rdsdb.tf
@@ -1,10 +1,10 @@
 #create a RDS Data Base
 resource "aws_db_instance" "db1" {
-  allocated_storage = 5
+  allocated_storage = 10
   engine = "mysql"
-  engine_version = "8.0.35"
-  instance_class = "db.t2.micro"
-  #name = "mydb1"
+  engine_version = "8.0"
+  instance_class = "db.t3.micro"
+  db_name = "mydb1"
   username = var.aws_db_username
   password = var.aws_db_password
   skip_final_snapshot = true
