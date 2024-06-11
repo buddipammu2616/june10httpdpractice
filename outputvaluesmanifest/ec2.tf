ebac3f6d5925b7e7181adff8fecfafd3cce63b32
mahesh <maheshkuraganti9@gmail.com>
2024-06-11 15:54:45 +0530

    commited some files

--- outputvaluesmanifest/ec2.tf
+++ outputvaluesmanifest/ec2.tf
@@ -1,7 +1,7 @@
 #Create EC2 instance
 resource "aws_instance" "maheshec2" {
-  ami = "ami-00fa32593b478ad6e"
-  instance_type = "t3.micro"
+  ami = var.aws_ami_id
+  instance_type = var.aws_instance_type
   key_name = "terraform-keypair"
   user_data = file("apacheinstall.sh")
 }
---
+++ outputvaluesmanifest/variables.tf
@@ -0,0 +1,25 @@
+#Create variables
+#create a variable for region
+variable "aws_region" {
+  description = "AWS Region created"
+  type = string
+  default = "ap-south-1"
+}
+#Create a variable for ami id
+variable "aws_ami_id" {
+  description = "AWS Ami id created"
+  type = string
+  default = "ami-00fa32593b478ad6e"
+}
+#Create a variable for aws instance count
+variable "aws_instance_count" {
+  description = "AWS instance count create"
+  type = number
+  default = 1
+}
+#Create a variable for instance type
+variable "aws_instance_type" {
+    description = "AWS instamce type created"
+   type = string
+   default = "t3.micro"
+}
