f7cbe344b293568d9ed9b2b6e5a78e91f85f90de
mahesh <maheshkuraganti9@gmail.com>
2024-06-11 12:10:29 +0530

    some content deleted and commited

--- dbmanifest/rdsdb.tf
+++ dbmanifest/rdsdb.tf
@@ -3,7 +3,7 @@ resource "aws_db_instance" "maheshDB" {
   allocated_storage = 5
   engine = "mysql"
   instance_class = "db.t2.micro"
-  name = "mydb1"
+  #name = "mydb1"
   username = var.aws_db_username
   password = var.aws_db_password
   skip_final_snapshot = true
