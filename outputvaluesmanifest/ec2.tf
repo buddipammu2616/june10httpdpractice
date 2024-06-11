85dc555677601704aa2d6594a36496a200305900
mahesh <maheshkuraganti9@gmail.com>
2024-06-11 16:01:54 +0530

    security group added in the ec2 instance

--- outputvaluesmanifest/ec2.tf
+++ outputvaluesmanifest/ec2.tf
@@ -4,4 +4,5 @@ resource "aws_instance" "maheshec2" {
   instance_type = var.aws_instance_type
   key_name = "terraform-keypair"
   user_data = file("apacheinstall.sh")
+  vpc_security_group_ids = [aws_security_group.maheshSGSSh.id]
 }
