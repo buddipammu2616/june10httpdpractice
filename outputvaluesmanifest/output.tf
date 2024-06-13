d80d2903b2145e7db4a503e8431edef6de52d552
mahesh <maheshkuraganti9@gmail.com>
2024-06-13 16:53:03 +0530

    again commited

--- outputvaluesmanifest/output.tf
+++ outputvaluesmanifest/output.tf
@@ -2,17 +2,17 @@
 #output for EC2 instance public IP
 output "aws_instance_publicip" {
   description = "EC2 instance public ip"
-  value = aws_instance.maheshec2.public_ip
+  value = aws_instance.maheshec2vm.public_ip
 }
 #output value for private IP
 output "aws_instance_privateip" {
   description = "EC2instance private ip"
-  value = aws_instance.maheshec2.private_ip
+  value = aws_instance.maheshec2vm.private_ip
 }
 #output value for SG
 output "security_group" {
   description = "Security Group"
-  value = aws_instance.maheshec2.security_groups
+  value = aws_instance.maheshec2vm.security_groups
 }
 #Output value for AMI Id
 output "amazom_ami_id" {
