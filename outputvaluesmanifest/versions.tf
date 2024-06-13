892ea161cc6b32890b75eb58c7a55ccb3551b67a
mahesh <maheshkuraganti9@gmail.com>
2024-06-13 20:45:46 +0530

    create a s3 bcakend for state storage and commited

--- outputvaluesmanifest/versions.tf
+++ outputvaluesmanifest/versions.tf
@@ -7,17 +7,12 @@ terraform {
         version = "~>5.0"
     }
   }
-  /*
-  #Create a backend for S3
+  #Create a S3 bucket for Backend
   backend "s3" {
     bucket = "buddipammukushi"
     region = "ap-south-1"
     key = "mahesh/terraform.tfstate"
-    dynamodb_table = "buddipammudynamodbtable"
-    encrypt = true
   }
-
-*/
 }
 #Provider Block
 provider "aws" {
