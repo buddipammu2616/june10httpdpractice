d5fad53a59dc7d2d1fe7cb435598a1b39df82f1b
mahesh <maheshkuraganti9@gmail.com>
2024-06-13 22:09:35 +0530

    commited

--- outputvaluesmanifest/ec2.tf
+++ outputvaluesmanifest/ec2.tf
@@ -1,13 +1,13 @@
 #Create EC2 instance
 # trunk-ignore(trivy/AVD-AWS-0028)
 # trunk-ignore(trivy/AVD-AWS-0131)
-resource "aws_instance" "maheshec2vm" {
+resource "aws_instance" "maheshec2vmnew" {
   ami = data.aws_ami.amazonlinuxamiid.id
   instance_type = var.aws_instance_type
   key_name = "terraform-keypair"
   user_data = file("apacheinstall.sh")
   vpc_security_group_ids = [aws_security_group.maheshSGSSh.id]
   tags = {
-    "Name" = "maheshec2vm"
+    "Name" = "maheshec2vmnew"
   }
 }
