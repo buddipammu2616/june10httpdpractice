33479c74d3421ef1584782841941f826859cf895
mahesh <maheshkuraganti9@gmail.com>
2024-06-13 16:50:29 +0530

    again commited

--- outputvaluesmanifest/ec2.tf
+++ outputvaluesmanifest/ec2.tf
@@ -1,11 +1,11 @@
 #Create EC2 instance
-resource "aws_instance" "maheshec2" {
+resource "aws_instance" "maheshec2vm" {
   ami = data.aws_ami.amazonlinuxamiid.id
   instance_type = var.aws_instance_type
   key_name = "terraform-keypair"
   user_data = file("apacheinstall.sh")
   vpc_security_group_ids = [aws_security_group.maheshSGSSh.id]
   tags = {
-    "Name" = "maheshec2"
+    "Name" = "maheshec2vm"
   }
 }
