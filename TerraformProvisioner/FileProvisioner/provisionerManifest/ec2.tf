c89f9d74634abb4584c0e3dce109bace8829c471
mahesh <maheshkuraganti9@gmail.com>
2024-06-15 14:02:25 +0530

    againcommited

--- TerraformProvisioner/FileProvisioner/provisionerManifest/ec2.tf
+++ TerraformProvisioner/FileProvisioner/provisionerManifest/ec2.tf
@@ -21,4 +21,23 @@ Connection {
   password = ""
   terraformkeypair = "file(terraformkeypair/terraformkey.pem)"
 }
+# Copies the file-copy.html file to /tmp/file-copy.html
+  provisioner "file" {
+    source      = "apps/file-copy.html"
+    destination = "/tmp/file-copy.html"
+  }
+  /*
+  # Copies the string in content into /tmp/file.log
+  provisioner "file" {
+    content     = "ami used: ${self.ami}" # Understand what is "self"
+    destination = "/tmp/file.log"
 }
+# Copies the app1 folder to /tmp - FOLDER COPY
+  provisioner "file" {
+    source      = "apps/app1"
+    destination = "/tmp"
+  }
+  provisioner "file" {
+    source      = "apps/app2/" # when "/" at the end is added - CONTENTS of FOLDER WILL BE COPIED
+    destination = "/tmp"
+  }*/
