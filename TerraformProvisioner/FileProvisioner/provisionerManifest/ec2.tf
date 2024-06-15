5161bd62410394a8a27a58e567ff8bf11b7c8cae
mahesh <maheshkuraganti9@gmail.com>
2024-06-15 11:27:48 +0530

    again commited

--- TerraformProvisioner/FileProvisioner/provisionerManifest/ec2.tf
+++ TerraformProvisioner/FileProvisioner/provisionerManifest/ec2.tf
@@ -1,7 +1,7 @@
 #Create a EC2 instance
 # trunk-ignore(tflint/terraform_required_providers)
 resource "aws_instance" "maheshec2" {
-  ami = "ami-id"
+  ami = data.aws_ami.amazonlinux.id
   instance_type = var.aws_instance_type
   key_name = "terraform-keypair"
   subnet_id = aws_subnet.maheshsubnet.id
