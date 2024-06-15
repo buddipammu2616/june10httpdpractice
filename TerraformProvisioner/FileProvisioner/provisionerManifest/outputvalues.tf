98773c4c4ffa5a8fa6fa183229dce9520e2f0314
mahesh <maheshkuraganti9@gmail.com>
2024-06-15 11:36:03 +0530

    output values created ad commited

---
+++ TerraformProvisioner/FileProvisioner/provisionerManifest/outputvalues.tf
@@ -0,0 +1,21 @@
+#Create output values
+#output value for creating public ip
+output "ec2_instance_publicip" {
+  description = "Ec2 instance public ip"
+  value = aws_instance.maheshec2.public_ip
+}
+#Output value for creating private ip
+output "ec2_instance_privateip" {
+  description = "EC2 instance private ip"
+  value = aws_instance.maheshec2.private_ip
+}
+#Output value for SG
+output "ec2_security_group" {
+  description = "EC2 SG"
+  value = aws_instance.maheshec2.security_groups
+}
+#output value for public DNS
+output "ec2_publicdns" {
+  description = "public dns url of ec2 instance"
+  value = "http://${aws_instance.maheshec2.public_dns}"
+}
