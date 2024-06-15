1ad051a3e56e8b89459d13260c165a557695c56a
mahesh <maheshkuraganti9@gmail.com>
2024-06-15 17:08:54 +0530

    ac

--- TerraformProvisioners/provisionersmanifest/ec2.tf
+++ TerraformProvisioners/provisionersmanifest/ec2.tf
@@ -29,7 +29,7 @@ resource "aws_instance" "maheshec2" {
     destination = "/tmp"
   }
   provisioner "file" {
-    source = "apps/app2/app2-file.html"
+    source = "apps/app2"
     destination = "/tmp"
   }
 }
