1336479088f0bc28b06b75f1448a577816245c59
mahesh <maheshkuraganti9@gmail.com>
2024-06-17 15:38:56 +0530

    in output variables arn created and commited

--- TerraformModules/S3StaticWebsite/outputvariables.tf
+++ TerraformModules/S3StaticWebsite/outputvariables.tf
@@ -4,3 +4,8 @@ output "aws_s3_bucket" {
   description = "aws_s3_bucket"
   value = aws_s3_bucket.buddipammukushi1.id
 }
+#Create a output value for ARN
+output "aws_s3_ARN" {
+  description = "aws_s3_ARN"
+  value = aws_s3_bucket.buddipammukushi1.arn
+}
